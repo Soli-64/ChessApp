63411082b9b6b7699c672855971257c538430d26
Soli-64 <soli64.games@gmail.com>
2025-11-15 18:39:17 +0100

    add: analysis move list, headers

--- src/pages/Analyzer.tsx
+++ src/pages/Analyzer.tsx
@@ -1,4 +1,4 @@
-import { Chess, Move, type Square } from "chess.js";
+import { Chess, type Square } from "chess.js";
 import { useEffect, useState } from "react";
 import { Chessboard, type ChessboardOptions } from "react-chessboard";
 import { invoke } from "versapy/api";
@@ -10,16 +10,27 @@ interface Analysis {
 }
 
 const AnalyzerView = () => {
+  
+  // game instance
   const [chess, setChess] = useState<Chess | null>(null);
-  const [moveList, setMoveList] = useState<string[]>([]); // SAN moves
+  
+  const [moveList, setMoveList] = useState<string[]>([]);
   const [currentMove, setCurrentMove] = useState(0);
   const [analysis, setAnalysis] = useState<Analysis | null>(null);
-  const [isAnalyzing, setIsAnalyzing] = useState(false);
+  
+  const [gameHeaders, setGameHeaders] = useState<null|Record<string,string>>(null) 
+  const [boardOrientation, setBoardOrientation] = useState<"white"|"black">("white")
 
   const [position, setPosition] = useState(chess?.fen())
+  
+  // Loader
+  const [loader, setLoader] = useState<boolean>(false);
 
   const evalToPercent = (evalStr: string): number => {
-    if (evalStr == "None") {
+    if (currentMove === 0) {
+      return 50 // first move, so return mid
+    }
+    if (evalStr === "None") { // Forced checkmate possible or checkmate
       return 0
     } else {
       const clean = evalStr.replace(/[+#]/g, "");
@@ -29,7 +40,6 @@ const AnalyzerView = () => {
     }
   };
 
-  // 1. Charger le PGN
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
@@ -41,9 +51,10 @@ const AnalyzerView = () => {
       game.loadPgn(text)
 
       setChess(game);
-
-      const players = chess?.getHeaders()
-      console.log(chess, players)
+      
+      const headers = game?.getHeaders()
+      console.log(headers)
+      setGameHeaders(headers || null)
 
       const moves = game.history();
       setMoveList(moves);
@@ -54,17 +65,16 @@ const AnalyzerView = () => {
     reader.readAsText(file);
   };
 
-  // 2. Aller à un coup
   const goToMove = async (index: number) => {
     if (!chess || index < 0 || index > moveList.length) return;
-
-    // Réinitialise + rejoue
+    if (loader) return;
+    
     chess.reset();
     for (let i = 0; i < index; i++) {
       const san = moveList[i];
       const move = chess.move(san);
       if (!move) {
-        console.error("Mouvement invalide:", san);
+        console.error("Invalid move:", san);
         return;
       }
     }
@@ -74,23 +84,21 @@ const AnalyzerView = () => {
     await analyzePosition();
   };
 
-  // 3. Analyse
   const analyzePosition = async () => {
-    setIsAnalyzing(true);
+    setLoader(true);
     try {
       const result = await invoke("analyze_position", { fen_data: chess?.fen() }) as Analysis;
       setAnalysis(result);
     } catch (err: any) {
-      console.error("Analyse échouée:", err);
-      setAnalysis({ eval: "Erreur", bestMove: null });
+      console.error("Analyzis failed:", err);
+      setAnalysis({ eval: "Error", bestMove: null });
     } finally {
-      setIsAnalyzing(false);
+      setLoader(false);
     }
   };
 
   useEffect(() => {
     if (chess && currentMove === 0) {
-      console.log("Position analyzed sent:", chess.fen())
       analyzePosition();
     }
   }, [chess]);
@@ -99,6 +107,7 @@ const AnalyzerView = () => {
 
   const BoardConfig: ChessboardOptions = {
     position: position,
+    boardOrientation,
     arrows: analysis?.bestMove ? [{
       startSquare: analysis.bestMove.substring(0, 2) as Square,
       endSquare: analysis.bestMove.substring(2, 4) as Square,
@@ -108,75 +117,81 @@ const AnalyzerView = () => {
 
   return (
     <div className="analyzer">
-      <div className="file-input">
-        <input type="file" accept=".pgn" onChange={handleFileChange} />
-      </div>
+      
+      {
+        !chess && 
+        <div className="file-input">
+          <input type="file" accept=".pgn" onChange={handleFileChange} />
+        </div>
+      }
 
       {chess && (
         <>
-          <div className="board">
-          <div className="eval-bar">
-            <div 
-              className="eval-fill"
-              style={{ 
-                height: `${evalToPercent(analysis?.eval || "0")}%` 
-              }}
-            />
+          <div className="analysis-left-container">
+            <div className="board-container">
+              <div className="eval-bar">
+                  <div 
+                    className="eval-fill"
+                    style={{ 
+                      height: `${evalToPercent(analysis?.eval || "0")}%` 
+                    }}>
+                  </div>
+                </div>
+              <div className="analysis-board">
+                <Chessboard options={BoardConfig} />
+              </div>
+              <div className="action-btns">
+                <button onClick={() => setBoardOrientation(boardOrientation === "black" ? "white" : "black")}>
+                  Sw
+                </button>
+              </div>
+            </div>
+
+            <div className="navigation">
+              <button onClick={() => goToMove(0)} disabled={currentMove === 0}>
+                Start
+              </button>
+              <button onClick={() => goToMove(currentMove - 1)} disabled={currentMove === 0}>
+                {"<"}
+              </button>
+
+              <span className="move-info">
+                {currentMove === 0 ? "Position initiale" : `Coup ${currentMove} / ${totalMoves}`}
+              </span>
+
+              <button onClick={() => goToMove(currentMove + 1)} disabled={currentMove === totalMoves}>
+                {">"}
+              </button>
+              <button onClick={() => goToMove(totalMoves)} disabled={currentMove === totalMoves}>
+                End
+              </button>
+            </div>
+
+            <div className="progress-bar">
+              <div
+                className="progress-fill"
+                style={{ width: `${(currentMove / totalMoves) * 100}%` }}
+              />
+            </div>
           </div>
-            <Chessboard options={BoardConfig} />
-          </div>
 
-          <div className="navigation">
-            <button onClick={() => goToMove(0)} disabled={currentMove === 0}>
-              Début
-            </button>
-            <button onClick={() => goToMove(currentMove - 1)} disabled={currentMove === 0}>
-              Précédent
-            </button>
-
-            <span className="move-info">
-              {currentMove === 0 ? "Position initiale" : `Coup ${currentMove} / ${totalMoves}`}
-            </span>
-
-            <button onClick={() => goToMove(currentMove + 1)} disabled={currentMove === totalMoves}>
-              Suivant
-            </button>
-            <button onClick={() => goToMove(totalMoves)} disabled={currentMove === totalMoves}>
-              Fin
-            </button>
-          </div>
-
-          <div className="progress-bar">
-            <div
-              className="progress-fill"
-              style={{ width: `${(currentMove / totalMoves) * 100}%` }}
-            />
-          </div>
-
-          <div className="analysis-panel">
-            <h3>Analyse {isAnalyzing && "(calcul...)"}</h3>
-            {analysis && (
+          <div className="analysis-right-container">
+            {
+              gameHeaders &&
               <>
-                <p>
-                  <strong>Évaluation :</strong>{" "}
-                  <span
-                    className={
-                      analysis.eval.startsWith("+")
-                        ? "positive"
-                        : analysis.eval.startsWith("-")
-                        ? "negative"
-                        : ""
-                    }
-                  >
-                    {analysis.eval}
-                  </span>
-                </p>
-                {analysis.bestMove && (
-                  <p><strong>Meilleur coup :</strong> {analysis.bestMove}</p>
-                )}
+                <div className="analysis-headers">
+                  <p>White: {gameHeaders.White} ({gameHeaders.WhiteElo})</p>
+                  <p>Black: {gameHeaders.Black} ({gameHeaders.BlackElo})</p>
+                  <div className="move-list">
+                    {moveList.slice(0, currentMove).map(m => (
+                      <p>{m}</p>
+                    ))}
+                  </div>
+                </div>
               </>
-            )}
+            }
           </div>
+
         </>
       )}
 
